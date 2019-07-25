364004c190ff6e8b514c3eaa471850137e613ed0
“Hindreen” <hndfaw@gmail.com>
2019-07-25 17:00:28 -0600

    Add container for matches

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import '../App.css';
+import './App.css';
 import { connect } from 'react-redux';
-import { fetchTodaysMatches } from '../apiCalls';
-import { setTodaysMatches } from '../actions';
-import  Header  from './header/Header'
+import { fetchTodaysMatches } from '../../apiCalls';
+import { setTodaysMatches } from '../../actions';
+import  Header  from '../header/Header'
+import  MatchesContainer  from '../matchesContainer/MatchesContainer'
 
 class App extends Component {
 
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <MatchesContainer />
       </div>
     );
   }
---
+++ src/containers/matchesContainer/MatchesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MatchesContainer extends Component {
+  render() {
+    return (
+      <div>
+        container
+      </div>
+    )
+  }
+}
+
+export default  MatchesContainer
