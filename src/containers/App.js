e93a2b94a9f35129ac5d8afec1cbec267259cb0f
“Hindreen” <hndfaw@gmail.com>
2019-07-25 16:12:49 -0600

    Setup header file

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import '../App.css';
 import { connect } from 'react-redux';
 import { fetchTodaysMatches } from '../apiCalls';
 import { setTodaysMatches } from '../actions';
+import  Header  from '../containers/Header'
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        header..
+      </div>
+    )
+  }
+}
+
+export default connect(null,null)(Header)
