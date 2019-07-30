45a14eac741c011e7add56930078d7d4bb55f21c
“Hindreen” <hndfaw@gmail.com>
2019-07-30 16:47:41 -0600

    Add test for action creators, today function and cleanData function

--- src/containers/app/App.test.js
+++ src/containers/app/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { App , mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
 import { setTodaysMatches, leaguesAction, setPremierLeague } from '../../actions';
+import { fetchTodaysMatches } from '../../apiCalls';
 
 
 
@@ -30,12 +31,84 @@ describe('App', ()=> {
   })
 
   it('should dispatch with a setPremierLeague action when handlePremierLeague is called', () => {
-  const mockDispatch = jest.fn();
-  const mockMatch = [{match: 'match'}];
-  const mockAction = setPremierLeague(mockMatch);
-  const mappedProps = mapDispatchToProps(mockDispatch);
-  mappedProps.handlePremierLeague(mockMatch);
-  expect(mockDispatch).toHaveBeenCalledWith(mockAction);
-})
+    const mockDispatch = jest.fn();
+    const mockMatch = [{match: 'match'}];
+    const mockAction = setPremierLeague(mockMatch);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.handlePremierLeague(mockMatch);
+    expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+  })
+
+  it('cleanMatches should return clean matches', () => {
+    const data = [
+      {
+        awayTeam: {team_id: 71, team_name: "Norwich", logo: "https://www.api-football.com/public/teams/71.png"},
+        elapsed: 0,
+        event_date: "2020-05-17T14:00:00+00:00",
+        event_timestamp: 1589724000,
+        firstHalfStart: null,
+        fixture_id: 157391,
+        goalsAwayTeam: null,
+        goalsHomeTeam: null,
+        homeTeam: {team_id: 50, team_name: "Manchester City", logo: "https://www.api-football.com/public/teams/50.png"},
+        league_id: 524,
+        referee: null,
+        round: "Regular Season - 38",
+        score: {halftime: null, fulltime: null, extratime: null, penalty: null},
+        secondHalfStart: null,
+        status: "Not Started",
+        statusShort: "NS",
+        venue: "Etihad Stadium (Manchester)",
+      }
+    ]
+    
+
+    const cleanData = [{
+      awayTeamLogo: "https://www.api-football.com/public/teams/71.png",
+      awayTeamName: "Norwich",
+      elapsed: 0,
+      event_date: "2020-05-17",
+      fixture_id: 157391,
+      goalsAwayTeam: null,
+      goalsHomeTeam: null,
+      homeTeamLogo: "https://www.api-football.com/public/teams/50.png",
+      homeTeamName: "Manchester City",
+      league_id: 524,
+      status: "Not Started",
+      statusShort: "NS"
+    }]
+
+    let wrapper = shallow(<App onDayDate={{date: '2019-01-01'}}  todaysMatches={[{match: 'match'}]}/>);
+  
+    const result = wrapper.instance().cleanMatches(data)
+    expect(result).toEqual(cleanData);
+  })
+
+  it.skip('componenetDidMoutn', () => {
+    const props = {handleTodaysMatches: jest.fn(), onDayDate: {date: '2019-01-01'}, todaysMatches: [{match: 'match'}]}
+    const fetchTodaysMatches = jest.fn()
+    let wrapper = shallow(<App {...props}/>);
+
+    wrapper.instance().componentDidMount()
+    expect(fetchTodaysMatches).toHaveBeenCalled()
+  })
+
+  it('today', () => {
+    const expected = () => {
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1;
+      let yyyy = today.getFullYear();
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+      return (yyyy + '-' + mm + '-' + dd);
+    }
+    let wrapper = shallow(<App onDayDate={{date: '2019-01-01'}}  todaysMatches={[{match: 'match'}]}/>);
+    expect(wrapper.instance().today()).toEqual(expected())
+  })
 
 })
